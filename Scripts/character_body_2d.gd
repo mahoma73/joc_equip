c3e50a4919f3dae52afca58a3939e8571609ae26
abfo2 <claverol.monserrat.pau@alumnes.fundaciocic.org>
2026-05-14 18:46:39 +0200

    Nivell 1 millorat
    arbres, altres detalls

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -19,8 +19,8 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
