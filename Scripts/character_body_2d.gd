3430520c9316974eac7b0efe3f51501cfc742bb2
Arni-gc <Gine.Carafi.Arnau@alumnes.fundaciocic.org>
2026-05-14 10:41:44 +0200

    Push_Mierda
    Tot va malament

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -20,13 +20,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	#else:
 		#velocity.x = move_toward(velocity.x, 0, SPEED)
-	anima(direction)
 	move_and_slide()
+	anima(velocity)
 
-func anima(direction):
-	if direction == "ui_right":
+func anima(velocitat: Vector2):
+	if velocitat.x > 0:
 		animated_sprite_2d.play("animacio_personatge")
 		animated_sprite_2d.flip_h = true
-	elif direction == "ui_left":
+	elif velocitat.x < 0:
 		animated_sprite_2d.play("animacio_personatge")
 		animated_sprite_2d.flip_h = false
