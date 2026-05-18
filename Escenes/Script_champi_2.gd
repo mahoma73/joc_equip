c7a9ea4ac705ced52a328368540103e67ed4613b
Arni-gc <Gine.Carafi.Arnau@alumnes.fundaciocic.org>
2026-05-18 23:51:25 +0200

    Això encara no està

--- Escenes/Script_champi_2.gd
+++ Escenes/Script_champi_2.gd
@@ -1,34 +1,50 @@
 extends CharacterBody2D
-var pot_fer_mal: bool = true
-const velocitat = 100.0
+
+const SPEED = 100.0
 var direccio = -1
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var pot_fer_mal: bool = true
+var pot_canviar_direccio: bool = true
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	velocity.x = direccio * velocitat
-	move_and_slide()
+
+	velocity.x = direccio * SPEED
+	
 	if is_on_wall():
 		direccio *= -1
+		
+	anima(velocity.x)
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var body = collision.get_collider()
+		
+		if body and body.name == "Jugador":
+			if pot_canviar_direccio:
+				pot_canviar_direccio = false
+				direccio *= -1
+				desenganxar_del_jugador()
+				
+			if body.playing and pot_fer_mal:
+				pot_fer_mal = false
+				body.restar_vida()
+				await get_tree().create_timer(1.0).timeout
+				pot_fer_mal = true
+
+func desenganxar_del_jugador() -> void:
+	await get_tree().create_timer(0.2).timeout
+	pot_canviar_direccio = true
 
 func anima(velocitat):
 	if velocitat > 0:
-		animated_sprite_2d.play("animacio_champi")
-		animated_sprite_2d.flip_h = false
+		$AnimatedSprite2D.play("animacio_champi")
+		$AnimatedSprite2D.flip_h = false
 	elif velocitat < 0:
-		animated_sprite_2d.play("animacio_champi")
-		animated_sprite_2d.flip_h = true
+		$AnimatedSprite2D.play("animacio_champi")
+		$AnimatedSprite2D.flip_h = true
 
 func _on_mort_champi_body_entered(body: Node2D) -> void:
 	if body.name == "Jugador":
 		queue_free()
-
-func _on_mort_jugador_body_entered(body: Node2D) -> void:
-	if body.name == "Jugador" and pot_fer_mal:
-		pot_fer_mal = false
-		var vides_jugador = body.vides
-		body.get_node("vides" + str(vides_jugador)).resta_vida()
-		body.restar_vida()
-		await get_tree().create_timer(1.0).timeout
-		pot_fer_mal = true
--- Escenes/Script_personatge_2.gd
+++ Escenes/Script_personatge_2.gd
@@ -1,33 +1,48 @@
 extends CharacterBody2D
+
 const SPEED = 300
 const JUMP_VELOCITY = -400.0
 var vides: int = 3
 var playing : bool = true
 var n = 0
+
 func _physics_process(delta: float) -> void:
 	if playing == false:
 		velocity.x = 0
 		if Input.is_action_just_pressed("Enter"):
 			get_tree().reload_current_scene()
 		return
+		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	move_and_slide()
 
 func restar_vida() -> void:
+	if playing == false:
+		return
+		
+	var nom_cor = "Vides_" + str(vides - 1)
+	if has_node(nom_cor):
+		get_node(nom_cor).queue_free()
+	
 	vides -= 1
-	if vides == 0:
+	if vides <= 0:
 		mor()
+
 func mor() -> void:
 	playing = false
 	$GO.text = "GAME OVER, press ENTER to try again"
+
 func _on_moneda_1_body_entered(body: Node2D) -> void:
 	n = n + 1
 	$Coins.text = "Monedes:" + str(n)
@@ -35,6 +50,7 @@ func _on_moneda_1_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	n = n + 1
 	$Coins.text = "Monedes:" + str(n)
+
 func _on_moneda_4_body_entered(body: Node2D) -> void:
 	n = n + 1
 	$Coins.text = "Monedes:" + str(n)
