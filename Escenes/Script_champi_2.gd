1eda36e755b313e625c2eb3ac736e7add528fb95
Jofretis4553 <Escur.Bacardit.Jofre@alumnes.fundaciocic.org>
2026-05-19 00:20:09 +0200

    JOC ACABAT
    VAMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Escenes/Script_champi_2.gd
+++ Escenes/Script_champi_2.gd
@@ -1,50 +1,34 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-var direccio = -1
 var pot_fer_mal: bool = true
-var pot_canviar_direccio: bool = true
+const VELOCITAT = 100.0
+var direccio = -1
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	velocity.x = direccio * SPEED
-	
+	velocity.x = direccio * VELOCITAT
+	move_and_slide()
 	if is_on_wall():
 		direccio *= -1
-		
 	anima(velocity.x)
-	move_and_slide()
-	
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var body = collision.get_collider()
-		
-		if body and body.name == "Jugador":
-			if pot_canviar_direccio:
-				pot_canviar_direccio = false
-				direccio *= -1
-				desenganxar_del_jugador()
-				
-			if body.playing and pot_fer_mal:
-				pot_fer_mal = false
-				body.restar_vida()
-				await get_tree().create_timer(1.0).timeout
-				pot_fer_mal = true
 
-func desenganxar_del_jugador() -> void:
-	await get_tree().create_timer(0.2).timeout
-	pot_canviar_direccio = true
-
-func anima(velocitat):
-	if velocitat > 0:
-		$AnimatedSprite2D.play("animacio_champi")
-		$AnimatedSprite2D.flip_h = false
-	elif velocitat < 0:
-		$AnimatedSprite2D.play("animacio_champi")
-		$AnimatedSprite2D.flip_h = true
+func anima(vel: float) -> void:
+	animated_sprite_2d.play("animacio_champi")
+	animated_sprite_2d.flip_h = vel < 0
 
 func _on_mort_champi_body_entered(body: Node2D) -> void:
 	if body.name == "Jugador":
 		queue_free()
+
+func _on_mort_jugador_body_entered(body: Node2D) -> void:
+	if body.name == "Jugador" and pot_fer_mal:
+		pot_fer_mal = false
+		var node_vida = body.get_node_or_null("vides" + str(body.vides))
+		if node_vida:
+			node_vida.restar_vida()
+		body.restar_vida()
+		await get_tree().create_timer(1.0).timeout
+		pot_fer_mal = true
